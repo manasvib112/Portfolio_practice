5bf34270187f1343c983f09982a5d4880319fe5e
manasvib112.gamil.com <manasvib112@gmail.com>
2020-01-10 17:31:11 +0530

    first commit

---
+++ pic/script.js
@@ -0,0 +1,17 @@
+
+
+const filterButtons=document.querySelector("#filter-btns").children;
+
+for(let i=0;i<filterButtons.length;i++){
+    filterButton[i].addEventListner("click",function(){
+        for(let j=0;j<filterButtons.length;j++){
+            filterButtons[j].classList.remove("active")
+        }
+    this.classList.add("active");
+    console.log(this.getAttribute("data-target")
+
+    for(let k=0;k<DataTransferItemList.length;k++)
+     item[k].style.display="none";
+}
+})
+}
